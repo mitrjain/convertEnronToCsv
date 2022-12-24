8862a9338a1ccacc99a125276d2fb1ad518661df
Mit <mitrjain@gmail.com>
2022-12-23 18:57:03 -0800

    Fix minor bug

--- executable/execute.sh
+++ executable/execute.sh
@@ -3,14 +3,14 @@
 # $1 - the value of this argument specifies the upper limit of the no. of emails to be read and hence written to the output csv dataset file. -1 indicates no limit.
 # $2 - the value of this argument specifies the upper limit of the no. of emails per user to be read and hence written to the output csv dataset file. -1 indicates no limit.
 
-if [ $# -ne 2 ];
+if [ $# -ne 1 ];
 then
    echo "**********$0: Missing arguments"
    echo "Argument information"
    echo " \$1 - the value of this argument specifies the upper limit of the no. of emails to be read and hence written to the output csv dataset file. -1 indicates no limit."
-   echo " \$2 - the value of this argument specifies the upper limit of the no. of emails per user to be read and hence written to the output csv dataset file. -1 indicates no limit."
+#    echo " \$2 - the value of this argument specifies the upper limit of the no. of emails per user to be read and hence written to the output csv dataset file. -1 indicates no limit."
 
   exit 1
 fi
 
-java -jar enron_to_csv-1.0.jar ../maildir ../structuredData/enronRawEmails.csv $1 $2
+java -jar enron_to_csv-1.0.jar ../maildir ../structuredData/enronRawEmails.csv $1 
