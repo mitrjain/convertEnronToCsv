716583fb13f695dedd2dd0193368d99985d48599
Mit <mitrjain@gmail.com>
2022-12-23 18:27:24 -0800

    Update readme

--- executable/createJar.sh
+++ executable/createJar.sh
@@ -1,6 +1,14 @@
 #! /bin/sh
 
+# $1 - path to the maven executable
+
+if [ $# -ne 1 ];
+then
+   echo "**********$0: Missing arguments"
+  exit 1
+fi
+
 rm enron_to_csv-1.0.jar
 cd ../enron_to_csv/
-/opt/homebrew/Cellar/maven/3.8.6/libexec/bin/mvn package -f pom.xml
+$1 package -f pom.xml
 cp target/enron_to_csv-1.0.jar ../executable/
--- executable/execute.sh
+++ executable/execute.sh
@@ -1,2 +1,16 @@
 #!/bin/sh
-java -jar enron_to_csv-1.0.jar ../maildir ../structuredData/enronRawEmails.csv 2000 50
+
+# $1 - the value of this argument specifies the upper limit of the no. of emails to be read and hence written to the output csv dataset file. -1 indicates no limit.
+# $2 - the value of this argument specifies the upper limit of the no. of emails per user to be read and hence written to the output csv dataset file. -1 indicates no limit.
+
+if [ $# -ne 2 ];
+then
+   echo "**********$0: Missing arguments"
+   echo "Argument information"
+   echo " \$1 - the value of this argument specifies the upper limit of the no. of emails to be read and hence written to the output csv dataset file. -1 indicates no limit."
+   echo " \$2 - the value of this argument specifies the upper limit of the no. of emails per user to be read and hence written to the output csv dataset file. -1 indicates no limit."
+
+  exit 1
+fi
+
+java -jar enron_to_csv-1.0.jar ../maildir ../structuredData/enronRawEmails.csv $1 $2
